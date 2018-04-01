2a30daf2f4202908c61c8d014bdea36874916c72
Glados <devilsparta@live.cn>
2018-04-01 18:39:57 +0800

    Player

---
+++ Player.js
@@ -0,0 +1,32 @@
+var JsBase = require ("./JsBase.js");
+
+function Player(){
+	this.m_PlayerNumber = null;
+	this.m_PlayerPos = [];
+	this.m_LastUpdateTime = JsBase.GetCurTime();
+}
+
+Player.prototype.NewPlayer = function(number, pos){	
+	this.m_PlayerNumber = number;
+	if(pos.length === 2){
+		this.m_PlayerPos = pos;
+	}	
+}
+
+Player.prototype.GetPos = function(){
+	return this.m_PlayerPos;
+}
+
+Player.prototype.SetPos = function(pos){
+	this.m_PlayerPos = pos;
+}
+
+Player.prototype.SetUpdateTime = function(time){
+	this.m_LastUpdateTime = time;
+}
+
+Player.prototype.GetUpdataTime = function(){
+	return this.m_LastUpdateTime;
+}
+
+module.exports = Player;
